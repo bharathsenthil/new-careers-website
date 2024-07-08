8393ab0282f9c109f1a436a3d6d1b1a402babeee
bharathsenthil <bharathrts@gmail.com>
2024-07-08 22:30:25 +0000

    Changed all app vars to flask_app in app.py file for easier use with deployment.

--- app.py
+++ app.py
@@ -29,13 +29,13 @@ JOBS = [
   }
 ]
 
-@app.route("/")
+@flask_app.route("/")
 def hello_world():
   return render_template('home.html', jobs=JOBS, company_name='VaseWorks')
 
-@app.route("/api/jobs")
+@flask_app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  flask_app.run(host='0.0.0.0', debug=True)
